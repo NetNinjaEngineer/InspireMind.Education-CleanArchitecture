86e302d695c8e7ac9bdfdc6c83f7e0da440e48f8
Mohamed ElHelaly <138399707+NetNinjaEngineer@users.noreply.github.com>
2024-08-23 19:53:15 +0300

    Created update user service (#3)

---
+++ InspireMind.Education.Application/Features/Users/Requests/Commands/UpdateUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using InspireMind.Education.Application.Bases;
+using InspireMind.Education.Application.DTOs.User;
+using MediatR;
+
+namespace InspireMind.Education.Application.Features.Users.Requests.Commands;
+public class UpdateUserCommand(Guid userId, UserForUpdateDto updateModel) : IRequest<Result<string>>
+{
+    public Guid UserId { get; } = userId;
+    public UserForUpdateDto UpdateModel { get; } = updateModel;
+}

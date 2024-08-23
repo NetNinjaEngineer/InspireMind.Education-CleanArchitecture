578be44a80506762e52f3d637ab9295d57332247
NetNinjaEngineer <memo5260287@outlook.com>
2024-08-23 19:39:08 +0300

    Implemented Registeration model and reset password model validations with localization

---
+++ InspireMind.Education.Application/Features/Authentication/Validators/RegisterModelValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using InspireMind.Education.Application.Models.Identity;
+using Microsoft.Extensions.Localization;
+
+namespace InspireMind.Education.Application.Features.Authentication.Validators;
+public class RegisterModelValidator : AbstractValidator<RegisterModel>
+{
+    private readonly IStringLocalizer<RegisterModelValidator> _localizer;
+
+    public RegisterModelValidator(IStringLocalizer<RegisterModelValidator> localizer)
+    {
+        _localizer = localizer;
+
+        RuleFor(x => x.FirstName)
+            .NotNull().WithMessage(string.Format(_localizer["notNull"], "First name"))
+            .NotEmpty().WithMessage(string.Format(_localizer["notEmpty"], "First name"))
+            .MaximumLength(50).WithMessage(string.Format(_localizer["maxLength"], "First name", 50));
+
+
+        RuleFor(x => x.LastName)
+            .NotNull().WithMessage(string.Format(_localizer["notNull"], "Last name"))
+            .NotEmpty().WithMessage(string.Format(_localizer["notEmpty"], "Last name"))
+            .MaximumLength(50).WithMessage(string.Format(_localizer["maxLength"], "Last name", 50));
+
+        RuleFor(x => x.UserName)
+            .NotNull().WithMessage(string.Format(_localizer["notNull"], "Username"))
+            .NotEmpty().WithMessage(string.Format(_localizer["notEmpty"], "Username"));
+
+        RuleFor(x => x.Email)
+            .NotNull().WithMessage(string.Format(_localizer["notNull"], "Email"))
+            .NotEmpty().WithMessage(string.Format(_localizer["notEmpty"], "Email"));
+
+        RuleFor(x => x.Password)
+            .NotNull().WithMessage(string.Format(_localizer["notNull", "Password"]))
+            .NotEmpty().WithMessage(string.Format(_localizer["notEmpty"], "Password"))
+            .MinimumLength(6).WithMessage(string.Format(_localizer["minLength"], "Password", 6))
+            .MaximumLength(100).WithMessage(string.Format(_localizer["maxLength"], 100));
+    }
+}
---
+++ InspireMind.Education.Application/Features/Users/Validators/UserForUpdateDtoValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using InspireMind.Education.Application.DTOs.User;
+using Microsoft.Extensions.Localization;
+
+namespace InspireMind.Education.Application.Features.Users.Validators;
+public class UserForUpdateDtoValidator : AbstractValidator<UserForUpdateDto>
+{
+    private readonly IStringLocalizer<UserForUpdateDtoValidator> _localizer;
+
+    public UserForUpdateDtoValidator(IStringLocalizer<UserForUpdateDtoValidator> localizer)
+    {
+        _localizer = localizer;
+
+        RuleFor(x => x.FirstName)
+          .NotNull().WithMessage(string.Format(_localizer["notNull"], "First name"))
+          .NotEmpty().WithMessage(string.Format(_localizer["notEmpty"], "First name"))
+          .MaximumLength(50).WithMessage(string.Format(_localizer["maxLength"], "First name", 50));
+
+
+        RuleFor(x => x.LastName)
+            .NotNull().WithMessage(string.Format(_localizer["notNull"], "Last name"))
+            .NotEmpty().WithMessage(string.Format(_localizer["notEmpty"], "Last name"))
+            .MaximumLength(50).WithMessage(string.Format(_localizer["maxLength"], "Last name", 50));
+
+        RuleFor(x => x.UserName)
+            .NotNull().WithMessage(string.Format(_localizer["notNull"], "Username"))
+            .NotEmpty().WithMessage(string.Format(_localizer["notEmpty"], "Username"));
+
+        RuleFor(x => x.Email)
+            .NotNull().WithMessage(string.Format(_localizer["notNull"], "Email"))
+            .NotEmpty().WithMessage(string.Format(_localizer["notEmpty"], "Email"));
+    }
+}
